9f75bb75926cccdfa1bbde794343cfda794fb297
Erick Müller <mullererick482@gmail.com>
2024-08-15 09:17:03 -0300

    Arvore_avl
    Agora insere evento contendo pessoa, carro, e pessoa com carro

--- gcet702/src/main/java/com/trabalho/resources/estruturas/Arvore_Avl.java
+++ gcet702/src/main/java/com/trabalho/resources/estruturas/Arvore_Avl.java
@@ -5,7 +5,7 @@ public class Arvore_Avl {
     public Nodo_avl raiz = null;
     
     public Nodo_avl inserir(Nodo_avl aux, int num ,  LS_Encadeada list, Hash_Map hash ) {
-       
+       //Evento com Pessoa
         Nodo_avl novo;
         if (aux == null) 
         {
@@ -40,7 +40,84 @@ public class Arvore_Avl {
         
         return aux;
     }
- 
+    
+    public Nodo_avl inserir(Nodo_avl aux, int num ,  LS_Encadeada list,LS_Encadeada list_carro ) {
+        //Evento com carro
+         Nodo_avl novo;
+         if (aux == null) 
+         {
+             novo = new Nodo_avl();
+             novo.num = num;
+             novo.list = list;
+             novo.list = list_carro;
+             novo.altd = 0;
+             novo.alte = 0;
+             novo.esq = null;
+             novo.dir = null;
+             aux = novo;
+         } 
+         else if (num < aux.num) 
+         {
+             aux.esq = inserir(aux.esq, num, list,list_carro);
+             if (aux.esq.altd > aux.esq.alte) {
+                 aux.alte = aux.esq.altd + 1;
+             } else {
+                 aux.alte = aux.esq.alte + 1;
+             }
+         } else {
+             aux.dir = inserir(aux.dir, num, list,list_carro);
+             if (aux.dir.altd > aux.dir.alte) {
+                 aux.altd = aux.dir.altd + 1;
+             } else {
+                 aux.altd = aux.dir.alte + 1;
+             }
+         }
+         
+         aux = balanceamento(aux);
+         
+         return aux;
+     } 
+
+
+    
+    public Nodo_avl inserir(Nodo_avl aux, int num ,  LS_Encadeada list,Hash_Map hash ,LS_Encadeada list_carro ) {
+       //Evento com Pessoa e Carro
+        Nodo_avl novo;
+        if (aux == null) 
+        {
+            novo = new Nodo_avl();
+            novo.num = num;
+            novo.list = list;
+            novo.hash = hash;
+            novo.list = list_carro;
+            novo.altd = 0;
+            novo.alte = 0;
+            novo.esq = null;
+            novo.dir = null;
+            aux = novo;
+        } 
+        else if (num < aux.num) 
+        {
+            aux.esq = inserir(aux.esq, num, list,hash,list_carro);
+            if (aux.esq.altd > aux.esq.alte) {
+                aux.alte = aux.esq.altd + 1;
+            } else {
+                aux.alte = aux.esq.alte + 1;
+            }
+        } else {
+            aux.dir = inserir(aux.dir, num, list,hash,list_carro);
+            if (aux.dir.altd > aux.dir.alte) {
+                aux.altd = aux.dir.altd + 1;
+            } else {
+                aux.altd = aux.dir.alte + 1;
+            }
+        }
+        
+        aux = balanceamento(aux);
+        
+        return aux;
+    } 
+
     public Nodo_avl balanceamento(Nodo_avl aux) 
     {
         int d, df;
